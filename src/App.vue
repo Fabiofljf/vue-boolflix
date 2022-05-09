d3b9060ba98a58089326f4a8029a5a2b6dcc3973
FabioLaForesta <fabiolaforesta@icloud.com>
2022-05-09 19:14:43 +0200

    Adding Milestone 2

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="app">
+    <input type="text" placeholder="movie or tv" v-model="type" />
     <input type="text" placeholder="search" v-model="search" />
     <button type="submit" @click="callApi" @keyup.enter="callApi">Search</button>
-    <div id="movie">
-      <ul v-for="(movie, index) in movies" :key="index">
-        <li>
+
+    <div id="movie" v-if="(this.type == 'movie')">
+      <ul>
+        <li v-for="(movie, index) in movies" :key="index">
           <h2>Titolo: {{movie.title}}</h2>
           <h3>Titolo originale: {{movie.original_title}}</h3>
-          <h4>Lingua: <country-flag country='{{movie.original_language}}' size='normal'/> </h4>
+          <h4>Lingua: <country-flag :country='getFlag(movie.original_language)' size='normal'/> </h4>
+          <h5>Voto: {{movie.vote_average}}</h5>
+        </li>
+      </ul>
+    </div>
+
+    <div id="serie" else>
+      <ul>
+        <li v-for="(movie, index) in movies" :key="index">
+          <h2>Titolo: {{movie.name}}</h2>
+          <h3>Titolo originale: {{movie.original_name}}</h3>
+          <h4>Lingua: <country-flag :country='getFlag(movie.original_language)' size='normal'/> </h4>
           <h5>Voto: {{movie.vote_average}}</h5>
         </li>
       </ul>
     </div>
   </div>
+
 </template>
 
 <script>
@@ -26,14 +40,16 @@ export default {
   },
   data() {
     return {
+      type: null,
       search: null,
       movies: null,
+      serietv: null
     };
   },
   methods: {
     callApi() {
       axios
-      .get(`https://api.themoviedb.org/3/search/movie?api_key=40a522c8e1eb2b9eb0188889f1def2c9&language=it-IT&page=1&include_adult=false&query=${this.search}`)
+      .get(`https://api.themoviedb.org/3/search/${this.type}?api_key=40a522c8e1eb2b9eb0188889f1def2c9&language=it-IT&page=1&include_adult=false&query=${this.search}`)
       .then((response) => {
         //console.log(response);
         //console.log(response.data.results);
@@ -41,7 +57,12 @@ export default {
         //console.log(this.movies);
       });
     },
+    getFlag(flag) {
+      if(flag == 'en') return 'gb-eng';
+      return flag;
+    }
   },
+  computed: {},
   mounted() {},
 };
 </script>
