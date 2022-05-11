a5d9688929841485ae08ec7d7d4d850ceea9aa0f
FabioLaForesta <fabiolaforesta@icloud.com>
2022-05-11 11:48:07 +0200

    Add Promise.all methods

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@
 
     <section id="cards">
       <div id="movie">
-          <div class="card" v-for="(movie, index) in this.movies" :key="index">
+          <div class="card" v-for="(movie, index) in this.movies.data.results" :key="index">
             <img :src="`http://image.tmdb.org/t/p/w500/${movie.poster_path}`" alt="Immagine di copertina">
             <div class="dettagli">
               <h5 class="p-1">Titolo: {{movie.title}}</h5>
@@ -103,6 +103,7 @@ export default {
           //console.log(response); // - Ottengo un array con dentro 2 array. La prima per i film la seconda pe rle serie
           //console.log(response[0]); 
           this.movies = response[0]; // - Arrays dei films
+          console.log(this.movies.data.results);
           this.serie = response[1]; // - Array delle serie
         });
     },
